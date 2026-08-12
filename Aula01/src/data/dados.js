04ce92b3bc1d2b38dbcc42a6505e1f21b8838090
João <jpo301007@gmail.com>
2026-08-12 19:29:07 -0300

    refactor: mudança da visualização do código

--- Aula01/src/data/dados.js
+++ Aula01/src/data/dados.js
@@ -3,7 +3,24 @@ import siriImg from '../images/siri.png';
 import roboImg from '../images/robo.png';
 
 export const dadosDosEquipamentos = [
-    { id: 1, imagemEquip: alexaImg, nome: "Alexa", status: "Funcionando", funcao: "Assistente Virtual" },
-    { id: 2, imagemEquip: siriImg, nome: "Siri", status: "Desligado", funcao: "Assistente Virtual" },
-    { id: 3, imagemEquip: roboImg, nome: "Robô Aspirador", status: "Em Manutenção", funcao: "Limpeza" },
+    {   id: 1, 
+        imagemEquip: alexaImg, 
+        nome: "Alexa", 
+        status: "Funcionando", 
+        funcao: "Assistente Virtual" 
+    },
+    { 
+        id: 2, 
+        imagemEquip: siriImg, 
+        nome: "Siri", 
+        status: "Desligado", 
+        funcao: "Assistente Virtual"
+    },
+    { 
+        id: 3, 
+        imagemEquip: roboImg, 
+        nome: "Robô Aspirador", 
+        status: "Em Manutenção", 
+        funcao: "Limpeza" 
+    },
 ];
